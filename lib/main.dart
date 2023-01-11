dc19d4472ea020c0bf5ea42faae5a29140e53b0e
Yashanki Tonde <83834396+Yashanki@users.noreply.github.com>
2023-01-11 17:38:37 +0530

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,6 +36,65 @@ class MyHomePage extends StatelessWidget {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
 
+    Future<List<Widget>> createList() async {
+      List<Widget> items = <Widget>[];
+      String datastring =
+          await DefaultAssetBundle.of(context).loadString("assets/data.json");
+      List<dynamic> dataJSON = jsonDecode(datastring);
+
+
+      dataJSON.forEach((object) {
+
+        String finalString = "";
+        List<dynamic> dataList = object["placeItems"];
+        dataList.forEach((item) {
+          finalString = finalString + item + "|";
+        });
+
+        items.add(Padding(padding: EdgeInsets.all(2.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0),),
+            boxShadow: [BoxShadow(
+                color: Colors.black12,
+              spreadRadius: 2.0,
+              blurRadius: 5.0,
+            )]
+          ),
+          margin: EdgeInsets.all(5.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomLeft: Radius.circular(10.0)),
+                child: Image.asset(object["placeImage"], width: 80, height: 80, fit: BoxFit.cover,),
+              ),
+              SizedBox(
+                width: 250,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 2.0, bottom: 2.0),
+                        child: Text(object["placeName"]),
+                      ),
+                      Text(finalString,overflow: TextOverflow.ellipsis, style: TextStyle(fontSize: 12.0, color: Colors.black54,),maxLines: 1,),
+                      Text("Min. Order: ${object["minOrder"]}", style: TextStyle(fontSize: 12.0, color: Colors.black54),),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),));
+      });
+      return items;
+    }
+
     return Scaffold(
       body: Container(
         height: screenHeight,
@@ -66,10 +128,34 @@ class MyHomePage extends StatelessWidget {
                   ),
                 ),
                 BannerwidgetArea(),
+                Container(
+                  child: FutureBuilder(
+                      initialData: <Widget>[Text(" ")],
+                      future: createList(),
+                      builder: (context,snapshot){
+                    if(snapshot.hasData){
+                      return Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: ListView(
+                          primary: false,
+                          shrinkWrap: true,
+                          children: <Widget>[...?snapshot.data],
+                        ),
+                      );
+                    }else {
+                      return CircularProgressIndicator();
+                    }
+                  })
+                )
               ],
             ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.black,
+        child: Icon(MdiIcons.food, color: Colors.white,),
       ),
     );
   }
@@ -94,21 +180,18 @@ class BannerwidgetArea extends StatelessWidget {
           child: Stack(
             fit: StackFit.expand,
             children: <Widget>[
-
               Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black38,
-                      offset: Offset(2.0,4.0),
-                      blurRadius: 5.0,
-                      spreadRadius: 1.0,
-                    )
-                  ]
-                ),
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black38,
+                        offset: Offset(2.0, 4.0),
+                        blurRadius: 5.0,
+                        spreadRadius: 1.0,
+                      )
+                    ]),
               ),
-
               ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(20.0)),
                 child: Image.asset(
@@ -116,18 +199,30 @@ class BannerwidgetArea extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
-
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(Radius.circular(20.0)),
                   gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.transparent, Colors.black]
-                  ),
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.transparent, Colors.black]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text(
+                      bannerItems[x],
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    ),
+                    Text("More than 40% Off",
+                        style: TextStyle(fontSize: 12.0, color: Colors.white)),
+                  ],
                 ),
               )
-
             ],
           ),
         ),
